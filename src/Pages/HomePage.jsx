b039a5670da330d2c73d5951219d40544c6ce6af
maineimissamu <sam.chadwick.p@gmail.com>
2025-03-18 20:54:32 +0100

    HomePage created

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Sidebar from '../components/Sidebar'
+
+
+function HomePage() {
+  return (
+    <div>
+        <Navbar />
+        <Sidebar />
+        <Footer />
+    </div>
+  )
+}
+
+export default HomePage
