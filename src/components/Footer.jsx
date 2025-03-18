428b5abde95de1879b48bbbf7817579538415109
Yarin <yarinleib@gmail.com>
2025-03-18 21:21:00 +0100

    Fotter-added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <a href="https://github.com/maineimissamu/kanban-board-app">GitHub repository</a>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
