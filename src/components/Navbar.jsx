99240f2851a5a39f7e65506254a57ec84094a98e
Yarin <yarinleib@gmail.com>
2025-03-20 19:31:44 +0100

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/kanban_board_logo.jpg';
+import '../styles/index.css';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <div className="background d-flex align-items-center justify-content-center">
+        <h3>TaskTrack</h3>
+        <img className="logo" src={logo} alt="logo"></img>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
